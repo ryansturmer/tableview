6dccc791c0823ee07a00f467f9c47c5da1e1eab7
Ryan Sturmer <ryansturmer@gmail.com>
2013-02-18 09:52:18 -0800

    Add distutils setup script.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='tableview',
+      version='1.0',
+      packages=['tableview'],
+      description='Package for loading and manipulating tabular data.',
+      author='Ryan Sturmer',
+      author_email='ryansturmer@gmail.com',
+      url='http://www.github.com/ryansturmer/tableview')
