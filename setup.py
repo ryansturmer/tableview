f5142c92e493542dfb6f7e5ccfeca556dab77ae5
Ryan Sturmer <ryansturmer@gmail.com>
2013-02-25 10:50:07 -0500

    Add tablib dependency

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
 
 setup(name='tableview',
       version='1.0.4',
@@ -6,4 +9,5 @@ setup(name='tableview',
       description='Library for loading and manipulating tabular data.',
       author='Ryan Sturmer',
       author_email='ryansturmer@gmail.com',
+      install_requires=['tablib'],
       url='http://www.github.com/ryansturmer/tableview')
