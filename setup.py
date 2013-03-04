66a2a72fd8b517d2de63664605b457c41a22164b
Ryan Sturmer <ryansturmer@gmail.com>
2013-03-04 07:30:18 -0500

    Integrate tests with setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,29 @@
 try:
-    from setuptools import setup
+    from setuptools import setup, Command
 except:
-    from distutils.core import setup
+    from distutils.core import setup, Command
 
 import tableview
+
+class PyTest(Command):
+    user_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        import sys,subprocess
+        errno = subprocess.call([sys.executable, 'runtests.py'])
+        raise SystemExit(errno)
+
 
 setup(name='tableview',
       version=tableview.__version__,
       packages=['tableview'],
+      cmdclass = {'test':PyTest},
       description='Library for loading and manipulating tabular data.',
       author='Ryan Sturmer',
       author_email='ryansturmer@gmail.com',
       install_requires=['tablib'],
+      tests_require=['pytest'],
       url='http://www.github.com/ryansturmer/tableview')
